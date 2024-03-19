3e2bc5607c7589549984c76fcc8fe54a50b2d7f5
Fernanda Kelly <fernandakrsx@outlook.com>
2024-03-19 12:53:05 -0300

    Base de dados 1.

--- Base_deDados_1.R
+++ Base_deDados_1.R
@@ -2,8 +2,10 @@ install.packages("basedosdados")
 library("basedosdados")
 
 # Defina o seu projeto no Google Cloud
-basedosdados::set_billing_id("<YOUR_PROJECT_ID>")
+basedosdados::set_billing_id("95803469120")
 
 # Para carregar o dado direto no R
 query <- bdplyr("br_mma_extincao.fauna_ameacada")
 df <- bd_collect(query)
+
+write.table(df, "~/IntroR_NUBEA/Dados/df.csv")
