f6d0fc51adce684fe513961c988bfa54a9561161
Fernanda Kelly <fernandakrsx@outlook.com>
2024-03-20 20:07:02 -0300

    Base de dados 2.

--- Base_deDados_2.R
+++ Base_deDados_2.R
@@ -2,7 +2,7 @@ install.packages("basedosdados")
 library("basedosdados")
 
 # Defina o seu projeto no Google Cloud
-set_billing_id("<YOUR_PROJECT_ID>")
+set_billing_id("95803469120")
 
 # Para carregar o dado direto no R
 query <- bdplyr("br_mapbiomas_estatisticas.classe")
