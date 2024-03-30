cea1c6b3932034ccbf7833142ed2624a8742548d
Fernanda Kelly <fernandakrsx@outlook.com>
2024-03-30 16:30:23 -0300

    Atualização.

---
+++ Step_1.R
@@ -0,0 +1,11 @@
+# Nome do codigo: Step_1.R
+# Autor: Fernanda Kelly Romeiro SIlva
+# Data de criacao: 2024-04-02
+# Data de ultima modificacao: 2024-04-03
+# Objetivos:
+#   - Aprender errando;
+#   - Brincar com as funcionalidades do RStudio
+
+# Preparando o R ----------------------------------------------------------
+
+
