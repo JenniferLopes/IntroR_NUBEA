c609737c573627b0b456477e6eb3e5e950427c4c
Fernanda Kelly <fernandakrsx@outlook.com>
2024-03-30 19:02:41 -0300

    Atualização.

--- Base_deDados_1.R
+++ Base_deDados_1.R
@@ -7,10 +7,11 @@ basedosdados::set_billing_id("95803469120")
 # Para carregar o dado direto no R
 query <- basedosdados::bdplyr("br_mma_extincao.fauna_ameacada")
 df_fauna_ameacada <- bd_collect(query)
-write.csv(df_fauna_ameacada, "~/IntroR_NUBEA/Dados/df_fauna_ameacada.csv")
-write.csv(df, "~/IntroR_NUBEA/Dados/df_teste.csv")
+utils::write.csv(df_fauna_ameacada, "/Área de Trabalho/Projetos R/IntroR_NUBEA/Dados")
 
 # Para carregar o dado direto no R
 query <- bdplyr("br_mma_extincao.flora_ameacada")
 df_flora_ameacada <- bd_collect(query)
-write.table(df_flora_ameacada, "~/IntroR_NUBEA/Dados/df_flora_ameacada.csv")
+utils::write.csv(df_flora_ameacada, "~/IntroR_NUBEA/Dados/df_flora_ameacada.csv")
+
+
